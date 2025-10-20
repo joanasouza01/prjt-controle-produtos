db1e2d50a096497a212ee92b0134b18eece9d52c
joana de souza <joanasouzasilva.2008@gmail.com>
2025-10-20 16:16:05 -0300

    fazendo api

--- back-end/api.py
+++ back-end/api.py
@@ -23,4 +23,24 @@ def colocar_produtos(nome: str, categoria: str, preco: float, quantidade: int):
     funcao.add_produtos(nome, categoria, preco, quantidade)
     return{"mensagem": "produto adicionado com sucesso!"}
 
-@app.
+@app.get("/produtos")
+def listar_produtos():
+    produtos = funcao.listar_produtos()
+    lista = []
+    for linha in produtos:
+        lista.append({"id": linha[0],
+                      "nome": linha[1],
+                      "categoria": linha[2],
+                      "preco": linha[3],
+                      "quantidade": linha[4]
+                    })
+    return{"produtos": lista}
+
+@app.put("/produtos/{id_produtos}")
+def atualizar_coisas(id_produtos: int, novo_preco: float, nova_quantidade: int):
+    produto = funcao.atualizar_preco_quantidade
+    if produto:
+        funcao.atualizar_preco_quantidade(id_produtos, novo_preco, nova_quantidade)
+        return{"mensagem": "produto atualizado com sucesso"}
+    else:
+        return{"erro": "produto não encontrado"}
